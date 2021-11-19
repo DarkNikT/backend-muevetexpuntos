3eba9f8116e23a95738dd0bc28f90de93ec87ab1
FabianGuevaraPinilla <fabianguevara157@gmail.com>
2021-11-19 14:50:48 -0500

    Carga imagenes

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static/images', express.static(path.join(__dirname,'../assets/images')));
 app.use(cors());
 //Mongo connection
 database.mongoConnect();
