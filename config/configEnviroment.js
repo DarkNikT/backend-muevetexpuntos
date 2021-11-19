343130b6d6f0839437f72f7d3b763163870fbcb8
FabianGuevaraPinilla <fabianguevara157@gmail.com>
2021-11-19 10:48:35 -0500

    update configuracion dev

--- config/configEnviroment.js
+++ config/configEnviroment.js
@@ -2,12 +2,13 @@
 // config.js
 const dotenv = require('dotenv').config();
 
-module.exports = {
 
+module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   HOST: process.env.HOST || '127.0.0.1',
-  PORT: process.env.PORT || 5000,
-  BD: process.env.BD || "bd_muevetexpuntos",
+  PORT: process.env.PORT || 3500,
+  PORT_BD: 27017,
+  BD: process.env.BD || "hr",
   USER_MONGO: process.env.USER_DATABASE || "user-pruebas",
   PASS_MONGO:  process.env.PASS_DATABASE || "WviaeBXWOMn5zMEg"
 }
--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
-const config = require("./configEnvirroment")
+const config = require("./configEnviroment")
 
 exports.mongoConnect = () =>{
-    const mongoStringConnectiont =`mongodb+srv://${config.USER_MONGO}:${config.PASS_MONGO}@cluster0.ainyd.mongodb.net/${config.BD}?retryWrites=true&w=majority`;
-    mongoose.connect(mongoStringConnectiont);
+    //const mongoStringConnectiont =`mongodb+srv://${config.USER_MONGO}:${config.PASS_MONGO}@cluster0.ainyd.mongodb.net/${config.BD}?retryWrites=true&w=majority`;
+    const mongoStringConnection = `mongodb://${config.HOST}:${config.PORT_BD}/${config.BD}`
+    mongoose.connect(mongoStringConnection);
     mongoose.Promise =global.Promise;
     const dbConnection =mongoose.connection;
     dbConnection.on("error", console.error.bind(console, "error en la conexion de mongodb"))
